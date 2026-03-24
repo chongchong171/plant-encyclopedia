d9277e345d189b80c020d069460ab28c93a9ad69
root <root@localhost.localdomain>
2026-03-25 01:52:01 +0800

    fix: 添加缺失的 formatRelativeTime 函数

--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,38 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+/**
+ * 格式化相对时间
+ */
+const formatRelativeTime = timestamp => {
+  if (!timestamp) return ''
+  
+  const now = Date.now()
+  const diff = now - timestamp
+  
+  const minute = 60 * 1000
+  const hour = 60 * minute
+  const day = 24 * hour
+  const week = 7 * day
+  const month = 30 * day
+  
+  if (diff < minute) {
+    return '刚刚'
+  } else if (diff < hour) {
+    return Math.floor(diff / minute) + '分钟前'
+  } else if (diff < day) {
+    return Math.floor(diff / hour) + '小时前'
+  } else if (diff < week) {
+    return Math.floor(diff / day) + '天前'
+  } else if (diff < month) {
+    return Math.floor(diff / week) + '周前'
+  } else {
+    const date = new Date(timestamp)
+    return (date.getMonth() + 1) + '月' + date.getDate() + '日'
+  }
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatRelativeTime: formatRelativeTime
 }
